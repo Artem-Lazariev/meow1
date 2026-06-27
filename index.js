38de2dba13fc6bea55e952a53f06ca879e1fee21
Artem-Lazariev <lazarievartem25@gmail.com>
2026-06-27 18:17:01 +0300

    Refactor moveBox function by removing comments
    Removed comments from the moveBox function and event listener.

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ const debouncedChangeSize = _.debounce(changeImageSize, 100);
 sliderInput.addEventListener('input', debouncedChangeSize);
 const box = document.getElementById('box');
 
-// Функція, яка змінює позицію квадрата під курсор
+
 function moveBox(event) {
-    // Віднімаємо 25 (половину ширини/висоти), щоб курсор був чітко по центру квадрата
     box.style.left = `${event.clientX - 25}px`;
     box.style.top = `${event.clientY - 25}px`;
 }
 
-// Використовуємо метод _.debounce з бібліотеки Lodash
 const debouncedMove = _.debounce(moveBox, 100);
 
-// Слухаємо переміщення миші по всьому вікну
 window.addEventListener('mousemove', debouncedMove);
